ead91a78e3fad9b6198196e89ab191336d36fbd2
xvavrik3 <xvavrik3@mail.muni.cz>
2021-05-24 18:39:42 +0200

    initial commit

---
+++ MatchOrganizer/MatchOrganizer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MatchOrganizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
